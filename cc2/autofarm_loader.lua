c68d8019d59b1fd178f7e0daff3ce26a7c1ebb59
fyz <36511274+yafyz@users.noreply.github.com>
2020-03-17 19:19:15 +0100

    Update autofarm_loader.lua

--- cc2/autofarm_loader.lua
+++ cc2/autofarm_loader.lua
@@ -1,7 +1,7 @@
 writefile("cc2autofarmv3.lua", game:HttpGet("https://raw.githubusercontent.com/yafyz/rbxscripts/master/cc2/autofarm.lua"))
 if not pcall(function() 
-    readfile("cc2autofarm_settings.lua")
+    readfile("cc2autofarm_settings.json")
 end) then 
-    writefile("cc2autofarm_settings.lua", [[{"Auto-Rebirth":false}]])
+    writefile("cc2autofarm_settings.json", [[{"Auto-Rebirth":false}]])
 end
 loadfile("cc2autofarmv3.lua")()
